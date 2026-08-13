ebc7629a76f9624a96d13140ba29893f0f72b201
leopold birungi <leopoldbirungi@gmail.com>
2026-08-13 11:53:02 +0300

    Expose tenant invitations in operator admin

--- apps/tenants/admin.py
+++ apps/tenants/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from apps.tenants.models import Tenant, TenantMembership
+from apps.tenants.models import Tenant, TenantInvitation, TenantMembership
 
 
 class TenantMembershipInline(admin.TabularInline):
@@ -22,3 +22,19 @@ class TenantMembershipAdmin(admin.ModelAdmin):
     list_display = ("tenant", "user", "role")
     list_filter = ("role",)
     search_fields = ("tenant__name", "tenant__slug", "user__username", "user__email")
+
+
+@admin.register(TenantInvitation)
+class TenantInvitationAdmin(admin.ModelAdmin):
+    list_display = (
+        "email",
+        "tenant",
+        "role",
+        "invited_by",
+        "expires_at",
+        "accepted_at",
+        "revoked_at",
+    )
+    list_filter = ("role", "accepted_at", "revoked_at")
+    search_fields = ("email", "tenant__name", "tenant__slug")
+    readonly_fields = ("token_hash", "created_at", "accepted_at", "revoked_at")
